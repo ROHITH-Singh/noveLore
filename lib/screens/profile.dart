54fe737e9edef22dd6c83d5caf479851737f4e72
ROHITH-Singh <rs9113848@gmail.com>
2021-02-26 20:12:28 +0530

    authentication part integration

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,13 +1,12 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:novel/screens/wrapper.dart';
+
 
 class profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-       color: Colors.redAccent,
-    );
+    return Wrapper();
   }
 }
